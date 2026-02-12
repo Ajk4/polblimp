49b3f5d2448b4cac90c71a8002e691a84f6f978d
ajk <ajk>
2026-02-12 09:07:58 +0100

    Add more tags to dictionary generation

--- generate_dictionary.main.py
+++ generate_dictionary.main.py
@@ -2,12 +2,25 @@ from tqdm import tqdm
 import csv, gzip
 from collections import defaultdict
 
-TAG_F = "praet:sg:f:perf"
-TAG_M = "praet:sg:m1.m2.m3:perf"
+# pri - pierwsza osoba
+# sec - druga osoba
+# ter - trzecia osoba
+
+TAGS = [
+    "fin:sg:pri:perf",
+    "fin:sg:sec:perf",
+    "fin:sg:ter:perf",
+    "praet:sg:f:perf",
+    "praet:sg:m1.m2.m3:perf"
+]
 
 acc = defaultdict(lambda: {})
 
+# TODO komentarz url
+# TODO argparse
 path = "PoliMorf-0.6.7.tab.gz"
+
+all_tags = set()
 
 with tqdm(gzip.open(path, "rt", encoding="utf-8", errors="replace")) as f:
     for line in f:
@@ -18,13 +31,19 @@ with tqdm(gzip.open(path, "rt", encoding="utf-8", errors="replace")) as f:
         if len(cols) < 3:
             continue
         form, lemma, tag = cols[0], cols[1], cols[2]
-        if tag in {TAG_F, TAG_M}:
+        if tag in TAGS:
             # Use only one option, ignore multi-option
             acc[lemma][tag] = form
 
-with open("dictionary.csv", "w", encoding="utf-8", newline="") as out:
+        all_tags.add(tag)
+
+print("All tags", all_tags)
+
+with open("dictionary.v2.csv", "w", encoding="utf-8", newline="") as out:
     w = csv.writer(out)
-    w.writerow([TAG_M, TAG_F])
+    w.writerow(["lemma", *TAGS])
     for lemma, d in acc.items():
-        if TAG_M in d and TAG_F in d:
-            w.writerow([d[TAG_M], d[TAG_F]])
+        row = [d.get(tag, "") for tag in TAGS]
+        has_at_least_on_tag = any([len(e) > 0 for e in row])
+        if has_at_least_on_tag:
+            w.writerow([lemma, *row])
