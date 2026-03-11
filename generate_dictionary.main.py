e1b7ab40ecbb886a91606651012e4e72f79081e2
ajk <ajk>
2026-03-11 12:12:46 +0100

    Dict generation - use blacklist instead for whitelist for tags

--- generate_dictionary.main.py
+++ generate_dictionary.main.py
@@ -2,81 +2,46 @@ from tqdm import tqdm
 import csv, gzip
 from collections import defaultdict
 
-# pri - first person
-# sec - second person
-# ter - third person
-
-# sg - singular
-# pl - plural
-
-fin_tags = []
-
-for t1 in ["sg", "pl"]:
-    for t2 in ["pri", "sec", "ter"]:
-        for t3 in ["perf", "imperf"]:
-            fin_tags.append(f"fin:{t1}:{t2}:{t3}")
-
-TAGS = [
-    *fin_tags,
-    'praet:sg:n1.n2:perf'
-    'praet:sg:n1.n2:imperf'
-    'praet:sg:n1.n2:imperf.perf'
-    'praet:sg:f:perf',
-    'praet:sg:f:imperf',
-    "praet:sg:m1.m2.m3:perf",
-    'praet:sg:m1.m2.m3:imperf',
-    'praet:sg:m1.m2.m3:imperf.perf',
-    'praet:pl:m1.p1:perf',
-    'praet:pl:m1.p1:imperf',
-    'praet:pl:m1.p1:imperf.perf',
-    'praet:pl:m2.m3.f.n1.n2.p2.p3:perf',
-    'praet:pl:m2.m3.f.n1.n2.p2.p3:imperf',
-    'praet:pl:m2.m3.f.n1.n2.p2.p3:imperf.perf',
-    'ppron3:sg:nom:m1.m2.m3:ter:_:_',
-]
-
-assert len(TAGS) == len(set(TAGS)), "No duplicate tags"
-
-acc = defaultdict(lambda: {})
-
 # TODO komentarz url
 # TODO argparse
 path = "PoliMorf-0.6.7.tab.gz"
 
+acc = defaultdict(lambda: {})
 all_tags = set()
 
 with tqdm(gzip.open(path, "rt", encoding="utf-8", errors="replace")) as f:
     for line in f:
         line = line.rstrip("\n")
         if not line:
-            continue
+            raise Exception("Empty line")
         cols = line.split("\t")
         if len(cols) < 3:
             continue
         form, lemma, tag = cols[0], cols[1], cols[2]
 
-        # HACK FIXME
+        unwanted_prefixes = {"adj", "adv:", 'aglt:', "depr:", "subst", 'ppron12', 'ppron3', 'prep:', 'ppas', 'pact:', 'ger:', 'imps:'}
+
+        # reduce dict by removing unneeded entries
+        if tag in {'interj', 'burk', 'comp', 'conj', 'qub'} or any(tag.startswith(p) for p in unwanted_prefixes):
+            continue
+
+        # HACK FIXME handle in second stage
         if tag.endswith(":nagl"):
             tag = tag[:-len(":nagl")]
 
-        if tag in TAGS:
-            # Use only one option, ignore multi-option
-            acc[lemma][tag] = form
+        acc[lemma][tag] = form
 
         all_tags.add(tag)
 
 print("All tags", all_tags)
+all_tags = list(sorted(all_tags))
 
-with open("conllu_analysis/dictionary.v3.csv", "w", encoding="utf-8", newline="") as out:
+with open("conllu_analysis/dictionary.v4.csv", "w", encoding="utf-8", newline="") as out:
     w = csv.writer(out)
-    w.writerow(["lemma", *TAGS])
+    w.writerow(["lemma", *all_tags])
     for lemma, d in acc.items():
-        row = [d.get(tag, "") for tag in TAGS]
-        has_at_least_on_tag = any([len(e) > 0 for e in row])
-        if has_at_least_on_tag:
-            w.writerow([lemma, *row])
+        row = [d.get(tag, "") for tag in all_tags]
+        has_at_least_one_tag = any([len(e) > 0 for e in row])
+        assert has_at_least_one_tag
 
-
-for tag in TAGS:
-    if tag not in all_tags:
-        print(f"unexpected tag {tag}")
+        w.writerow([lemma, *row])
