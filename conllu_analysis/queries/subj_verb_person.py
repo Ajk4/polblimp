8f745f45e1db30dc0ea246fc02f42e9ce2eaeece
ajk <ajk>
2026-04-10 16:33:22 +0200

    WIP

--- conllu_analysis/queries/subj_verb_person.py
+++ conllu_analysis/queries/subj_verb_person.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import random
 from typing import Optional
 
 import conllu
@@ -50,9 +51,28 @@ def run_subj_verb_person(sentences: list[conllu.TokenList], morph_dict: MorphDic
         progress_desc="subj_verb_person__2",
     )
 
-    df = pd.concat([variant_1, variant_2])
+    variant_3 = run_filter_transform(
+        sentences,
+        match_subj_verb_person__3,
+        append_aux_clitic,
+        limit=limit,
+        progress_desc="subj_verb_person__3",
+    )
+
+    df = pd.concat([variant_1, variant_2, variant_3])
+    df = variant_3
     df.attrs["matched_sentences"] = len(variant_1) + len(variant_2)
     return df
+
+
+def append_aux_clitic(sentence: conllu.TokenList) -> bool:
+    root = sentence.to_tree().token
+    root_form = root['form']
+    if random.randint(0, 1) == 0:
+        root['form'] = root_form + "em"
+    else:
+        root['form'] = root_form + "eś"
+    return True
 
 def remove_aux_clitic(sentence: conllu.TokenList) -> bool:
     root = sentence.to_tree().token
@@ -126,5 +146,27 @@ def match_subj_verb_person__2(
             for clitic in root.children:
                 if clitic.token["deprel"] == "aux:clitic":
                     return True
+
+    return False
+
+
+def match_subj_verb_person__3(
+        sentence: conllu.TokenList,
+) -> bool:
+    root = sentence.to_tree()
+    if not (root.token["deprel"] == 'root' and root.token["upos"] == "VERB"):
+        return False
+
+    root_feats = root.token['feats']
+
+    for nsubj in root.children:
+        nsubj_token = nsubj.token
+        nsubj_feats = nsubj_token['feats']
+
+        if not (nsubj_token["upos"] == "PRON" and nsubj_token["deprel"] == "nsubj"):
+            continue
+
+        if root_feats.get('Tense', '') in {"Past"} and nsubj_feats.get('Person', '') in {'3'}:
+            return True
 
     return False
